d485bb31a9e38dd69ec95dd71ab3f75abed2908c
aliahmadcse <aliahmadcse@gmail.com>
2020-01-05 16:03:50 +0500

    Adds url parameter and show.php file

--- public/staff/index.php
+++ public/staff/index.php
@@ -7,7 +7,7 @@
     <div id="main-menu">
         <h2>Main Menu</h2>
         <ul>
-            <li><a href="./subjects/index.php">Subjects</a></li>
+            <li><a href="<?= url_for('/staff/subjects/index.php'); ?>">Subjects</a></li>
         </ul>
     </div>
 </div>
