58e3c7a8a0b1dc2241466f6f0c6ca3ffea2822ae
aliahmadcse <aliahmadcse@gmail.com>
2020-01-15 03:57:36 +0500

    Adds validation functions

--- private/initialize.php
+++ private/initialize.php
@@ -19,5 +19,6 @@ define('WWW_ROOT', $doc_root);
 require_once('functions.php');
 require_once('database.php');
 require_once('query_functions.php');
+require_once('validation_functions.php');
 
-$db=db_connect();
+$db = db_connect();
