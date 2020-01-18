70801507f44dc6f334d33742434feff1e3f20603
aliahmadcse <aliahmadcse@gmail.com>
2020-01-18 12:31:03 +0500

    Changes db credentials

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
 
-define("DB_SERVER",'localhost');
-define("DB_USER",'webuser');
-define("DB_PASS",'244623');
-define("DB_NAME",'globe_bank');
+define("DB_SERVER", 'localhost');
+define("DB_USER", 'root');
+define("DB_PASS", '');
+define("DB_NAME", 'globe_bank');
