50ef9f95a9b7d1b68f77e85831a88081f27d9b1c
aliahmadcse <aliahmadcse@gmail.com>
2020-01-19 22:12:02 +0500

    Adds appropriate comments

--- private/validation_functions.php
+++ private/validation_functions.php
@@ -107,8 +107,8 @@ function has_unique_page_menu_name($menu_name, $current_id = 0)
 {
   global $db;
   $sql = "SELECT * FROM pages ";
-  $sql .= "WHERE menu_name='" . db_escape($db,$menu_name) . "' ";
-  $sql .= "AND id!='" . db_escape($db,$current_id) . "'";
+  $sql .= "WHERE menu_name='" . db_escape($db, $menu_name) . "' ";
+  $sql .= "AND id!='" . db_escape($db, $current_id) . "'";
   $page_set = mysqli_query($db, $sql);
   $page_count = mysqli_num_rows($page_set);
   mysqli_free_result($page_set);
@@ -116,12 +116,13 @@ function has_unique_page_menu_name($menu_name, $current_id = 0)
   return $page_count === 0;
 }
 
-//a function to check if a subject has any associated pages
+//a function to check if a subject has any associated pages or
+// not
 function has_any_assciated_page($subject_id)
 {
   global $db;
   $sql = "SELECT * FROM pages ";
-  $sql .= "WHERE subject_id='" . db_escape($db,$subject_id) . "'";
+  $sql .= "WHERE subject_id='" . db_escape($db, $subject_id) . "'";
   $page_set = mysqli_query($db, $sql);
   $page_count = mysqli_num_rows($page_set);
   mysqli_free_result($page_set);
