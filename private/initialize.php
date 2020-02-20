418f6f5fd25866c13aff0fac747e38f3c4daaecb
aliahmadcse <aliahmadcse@gmail.com>
2020-02-21 00:13:42 +0500

    Start version 2 of the project

--- private/initialize.php
+++ private/initialize.php
@@ -14,6 +14,7 @@ define('SHARED_PATH', PRIVATE_PATH . '/shared');
 
 $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
+
 define('WWW_ROOT', $doc_root);
 
 require_once('functions.php');
