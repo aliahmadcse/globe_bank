ad51a49590dfacd72c30f3ee333bbecf9f69f199
aliahmadcse <aliahmadcse@gmail.com>
2020-02-25 19:00:07 +0500

    Adds the feature the display the first page of selected subject

--- public/index.php
+++ public/index.php
@@ -1,18 +1,24 @@
 <?php require_once '../private/initialize.php'; ?>
 
-<?php if (isset($_GET['subject_id'])) {
+<?php
+if (isset($_GET['subject_id'])) {
     $subject_id = $_GET['subject_id'];
-} ?>
-
-
-<?php if (isset($_GET['id'])) {
+    $page_set = find_pages_by_subject_id($subject_id);
+    $page = mysqli_fetch_assoc($page_set);
+    mysqli_free_result($page_set);
+    if (!$page) {
+        redirect_to(url_for('./index.php'));
+    }
+    $page_id = $page['id'];
+} elseif (isset($_GET['id'])) {
     $page_id = $_GET['id'];
     $page = find_page_by_id($page_id);
     if (!$page) {
         redirect_to(url_for('/index.php'));
     }
     $subject_id = $page['subject_id'];
-} ?>
+}
+?>
 
 
 
