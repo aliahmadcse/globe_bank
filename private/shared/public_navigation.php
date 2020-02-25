304cc55dcc9a3373bc7f93c8c6116e78ee8af68b
aliahmadcse <aliahmadcse@gmail.com>
2020-02-25 18:44:13 +0500

    Adds feature to collapse nav bar

--- private/shared/public_navigation.php
+++ private/shared/public_navigation.php
@@ -10,28 +10,30 @@ $subject_id = $subject_id ?? '';
       <li class="<?php if ($nav_subject['id'] == $subject_id) {
                     echo "selected";
                   } ?>">
-        <a href="<?php echo url_for('index.php'); ?>">
+        <a href="<?php echo url_for('index.php?subject_id=' . h(u($nav_subject['id']))); ?>">
           <?php echo h($nav_subject['menu_name']); ?>
         </a>
 
-        <?php $nav_pages = find_pages_by_subject_id($nav_subject['id']); ?>
-        <ul class="pages">
-          <?php while ($nav_page = mysqli_fetch_assoc($nav_pages)) { ?>
-            <li class="<?php if ($nav_page['id'] == $page_id) {
-                          echo "selected";
-                        } ?>">
-              <a href="<?php echo url_for('index.php?id=' . h(u($nav_page['id']))); ?>">
-                <?php echo h($nav_page['menu_name']); ?>
-              </a>
+        <?php if ($nav_subject['id'] == $subject_id) { ?>
+          <?php $nav_pages = find_pages_by_subject_id($nav_subject['id']); ?>
+          <ul class="pages">
+            <?php while ($nav_page = mysqli_fetch_assoc($nav_pages)) { ?>
+              <li class="<?php if ($nav_page['id'] == $page_id) {
+                            echo "selected";
+                          } ?>">
+                <a href="<?php echo url_for('index.php?id=' . h(u($nav_page['id']))); ?>">
+                  <?php echo h($nav_page['menu_name']); ?>
+                </a>
 
-            </li>
-          <?php } // while $nav_pages
-          ?>
-        </ul>
-        <?php mysqli_free_result($nav_pages); ?>
-
+              </li>
+            <?php } // while $nav_pages
+            ?>
+          </ul>
+          <?php mysqli_free_result($nav_pages); ?>
+        <?php } ?>
       </li>
-    <?php } // while $nav_subjects 
+    <?php }
+    // while $nav_subjects 
     ?>
   </ul>
   <?php mysqli_free_result($nav_subjects); ?>
--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php require_once '../private/initialize.php'; ?>
 
+<?php if (isset($_GET['subject_id'])) {
+    $subject_id = $_GET['subject_id'];
+} ?>
+
+
 <?php if (isset($_GET['id'])) {
     $page_id = $_GET['id'];
     $page = find_page_by_id($page_id);
