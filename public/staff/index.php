942ed7b7adc318352b7a181140b76d4eefe89585
aliahmadcse <aliahmadcse@gmail.com>
2020-02-29 17:15:57 +0500

    Adds pages for the admin management

--- public/staff/index.php
+++ public/staff/index.php
@@ -9,6 +9,7 @@
         <ul>
             <li><a href="<?= url_for('/staff/subjects/index.php'); ?>">Subjects</a></li>
             <li><a href="<?= url_for('/staff/pages/index.php'); ?>">Pages</a></li>
+            <li><a href="<?= url_for('/staff/admins/index.php'); ?>">Admins</a></li>
         </ul>
     </div>
 </div>
