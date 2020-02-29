e0eeecdbadcb1c65c186743680e80f874a998163
aliahmadcse <aliahmadcse@gmail.com>
2020-02-29 18:27:18 +0500

    Adds user authentication

--- public/staff/login.php
+++ public/staff/login.php
@@ -10,9 +10,38 @@ if(is_post_request()) {
   $username = $_POST['username'] ?? '';
   $password = $_POST['password'] ?? '';
 
-  $_SESSION['username'] = $username;
+  // Validations
+  if(is_blank($username)) {
+    $errors[] = "Username cannot be blank.";
+  }
+  if(is_blank($password)) {
+    $errors[] = "Password cannot be blank.";
+  }
 
-  redirect_to(url_for('/staff/index.php'));
+  // if there were no errors, try to login
+  if(empty($errors)) {
+    // Using one variable ensures that msg is the same
+    $login_failure_msg = "Log in was unsuccessful.";
+
+    $admin = find_admin_by_username($username);
+    if($admin) {
+
+      if(password_verify($password, $admin['hashed_password'])) {
+        // password matches
+        log_in_admin($admin);
+        redirect_to(url_for('/staff/index.php'));
+      } else {
+        // username found, but password does not match
+        $errors[] = $login_failure_msg;
+      }
+
+    } else {
+      // no username found
+      $errors[] = $login_failure_msg;
+    }
+
+  }
+
 }
 
 ?>
