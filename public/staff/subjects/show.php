37f565dfa6f08bd1ff09c082c0a1b66ba1dfa60b
aliahmadcse <aliahmadcse@gmail.com>
2020-03-06 17:25:54 +0500

    Adds pages below the subjects

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -6,7 +6,8 @@ require_login();
 // $id = isset($_GET['id']) ? $_GET['id'] : '1';
 $id = $_GET['id'] ?? '1'; // PHP > 7.0
 
-$subject=find_subject_by_id($id);
+$subject = find_subject_by_id($id);
+$page_set = find_pages_by_subject_id($id);
 
 ?>
 
@@ -32,6 +33,45 @@ $subject=find_subject_by_id($id);
                 <dd><?php echo $subject['visible'] == '1' ? 'true' : 'false'; ?></dd>
             </dl>
         </div>
+        <hr>
+
+        <div class="pages listing">
+            <h2>Pages</h2>
+
+            <div class="actions">
+                <a class="action" href="<?php echo url_for('/staff/pages/new.php'); ?>">Create New Page</a>
+            </div>
+
+            <table class="list">
+                <tr>
+                    <th>ID</th>
+                    <th>Position</th>
+                    <th>Visible</th>
+                    <th>Name</th>
+                    <th>&nbsp;</th>
+                    <th>&nbsp;</th>
+                    <th>&nbsp;</th>
+                </tr>
+
+                <?php while ($page = mysqli_fetch_assoc($page_set)) { ?>
+                    <?php $subject = find_subject_by_id($page['subject_id']); ?>
+                    <tr>
+                        <td><?php echo h($page['id']); ?></td>
+                        <td><?php echo h($page['position']); ?></td>
+                        <td><?php echo $page['visible'] == 1 ? 'true' : 'false'; ?></td>
+                        <td><?php echo h($page['menu_name']); ?></td>
+                        <td><a class="action" href="<?php echo url_for('/staff/pages/show.php?id=' . h(u($page['id']))); ?>">View</a></td>
+                        <td><a class="action" href="<?php echo url_for('/staff/pages/edit.php?id=' . h(u($page['id']))); ?>">Edit</a></td>
+                        <td><a class="action" href="<?php echo url_for('/staff/pages/delete.php?id=' . h(u($page['id']))); ?>">Delete</a></td>
+                    </tr>
+                <?php } ?>
+            </table>
+
+            <?php mysqli_free_result($page_set); ?>
+
+        </div>
+
+
     </div>
 </div>
 
